88dae33a03dd0ae976b3b443e58372eef16cb8c0
dpmeltz <45836188+dpmeltz@users.noreply.github.com>
2019-02-20 16:54:25 -0500

    tweaktest

--- trumpTrack/server.R
+++ trumpTrack/server.R
@@ -14,52 +14,59 @@ library(lubridate)
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-  approval_topline <- read_csv("./data/approval_topline.csv")
-  approval_topline$modeldate <- mdy(approval_topline$modeldate)
+#  approval_topline <- read_csv("./data/approval_topline.csv")
+#  approval_topline$modeldate <- mdy(approval_topline$modeldate)
 
-  approval_summary <- approval_topline %>%
-    filter(subgroup == "Voters")
-  ylim <- c(min(approval_summary$approve_estimate),max(approval_summary$approve_estimate))
+#  approval_summary <- approval_topline %>%
+#    filter(subgroup == "Voters")
+#  ylim <- c(min(approval_summary$approve_estimate),max(approval_summary$approve_estimate))
 
 
-  tweets <- read_csv("./data/tweets.csv")
-  tweets$date <- date(mdy_hm(tweets$created_at))
+#  tweets <- read_csv("./data/tweets.csv")
+#  tweets$date <- date(mdy_hm(tweets$created_at))
 
-  tweet_summary <- tweets %>%
-    mutate(len = nchar(text)) %>%
-    group_by(date) %>%
-    summarize(n = n(), avg_len = mean(len, na.rm = TRUE), sd_len = sd(len, na.rm=TRUE))
+#  tweet_summary <- tweets %>%
+#    mutate(len = nchar(text)) %>%
+#    group_by(date) %>%
+#    summarize(n = n(), avg_len = mean(len, na.rm = TRUE), sd_len = sd(len, na.rm=TRUE))
 
   output$approval_plot <- renderPlot({
 
+    dummy <- c(1,2,5,3,6,4)
 
-    plot1 <- ggplot(approval_summary, aes(y = approve_estimate, x = modeldate)) +
-      geom_point(alpha = 0.25) + geom_smooth(span = 0.25, se = TRUE, color = "orange") +
-      geom_hline(aes(yintercept = mean(approve_estimate, na.rm = TRUE)), color = "darkred", alpha = .33) +
-      scale_x_date(date_breaks = "3 months", date_labels = "%y-%m-%d") +
-      ylim(ylim) +
-      xlim(input$date) +
-      xlab("Date") +
-      ylab("FiveThirtyEight Approval Estimate") +
-      theme(axis.text.x = element_text(angle = 60, hjust = 1),
-            panel.grid = element_blank(),
-            panel.background = element_blank())
+    plot1 <- plot(dummy)
+
+#      ggplot(approval_summary, aes(y = approve_estimate, x = modeldate)) +
+#      geom_point(alpha = 0.25) + geom_smooth(span = 0.25, se = TRUE, color = "orange") +
+#      geom_hline(aes(yintercept = mean(approve_estimate, na.rm = TRUE)), color = "darkred", alpha = .33) +
+#      scale_x_date(date_breaks = "3 months", date_labels = "%y-%m-%d") +
+#      ylim(ylim) +
+#      xlim(input$date) +
+#      xlab("Date") +
+#      ylab("FiveThirtyEight Approval Estimate") +
+#      theme(axis.text.x = element_text(angle = 60, hjust = 1),
+#            panel.grid = element_blank(),
+#            panel.background = element_blank())
     plot1
   })
 
   output$tweet_plot <- renderPlot({
 
-    plot2 <- ggplot(tweet_summary, aes(x = date, y = n)) +
-      geom_point(alpha = 0.25) + geom_smooth(span = 0.25, se = TRUE, color = "orange") +
-      geom_hline(aes(yintercept = mean(n, na.rm = TRUE)), color = "darkred", alpha = .33)+
-      scale_x_date(date_breaks = "3 months", date_labels = "%y-%m-%d") +
-      ylim(0,max(tweet_summary$n)) +
-      xlim(input$date) +
-      xlab("Date") +
-      ylab("Number of Tweets") +
-      theme(axis.text.x = element_text(angle = 60, hjust = 1),
-            panel.grid = element_blank(),
-            panel.background = element_blank())
+    dummy <- c(1,2,5,3,6,4)
+
+    plot2 <- plot(dummy)
+
+#      ggplot(tweet_summary, aes(x = date, y = n)) +
+#      geom_point(alpha = 0.25) + geom_smooth(span = 0.25, se = TRUE, color = "orange") +
+#      geom_hline(aes(yintercept = mean(n, na.rm = TRUE)), color = "darkred", alpha = .33)+
+#      scale_x_date(date_breaks = "3 months", date_labels = "%y-%m-%d") +
+#      ylim(0,max(tweet_summary$n)) +
+#      xlim(input$date) +
+#      xlab("Date") +
+#      ylab("Number of Tweets") +
+#      theme(axis.text.x = element_text(angle = 60, hjust = 1),
+#            panel.grid = element_blank(),
+#            panel.background = element_blank())
     plot2
 
   })
