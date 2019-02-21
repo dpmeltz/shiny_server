7492882c957e45153ce00de3afa668e026a424d0
Dan M <dpmeltz@gmail.com>
2019-02-20 19:32:05 -0500

    tweak7

--- trumpTrack/server.R
+++ trumpTrack/server.R
@@ -10,6 +10,7 @@
 library(ggplot2)
 library(dplyr)
 library(lubridate)
+library(readr)
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
@@ -33,21 +34,18 @@ server <- function(input, output) {
 
   output$approval_plot <- renderPlot({
 
-    dummy <- c(1,2,5,3,6,4)
 
-    plot1 <- plot(dummy)
-
-#      ggplot(approval_summary, aes(y = approve_estimate, x = modeldate)) +
-#      geom_point(alpha = 0.25) + geom_smooth(span = 0.25, se = TRUE, color = "orange") +
-#      geom_hline(aes(yintercept = mean(approve_estimate, na.rm = TRUE)), color = "darkred", alpha = .33) +
-#      scale_x_date(date_breaks = "3 months", date_labels = "%y-%m-%d") +
-#      ylim(ylim) +
-#      xlim(input$date) +
-#      xlab("Date") +
-#      ylab("FiveThirtyEight Approval Estimate") +
-#      theme(axis.text.x = element_text(angle = 60, hjust = 1),
-#            panel.grid = element_blank(),
-#            panel.background = element_blank())
+    plot1 <- ggplot(approval_summary, aes(y = approve_estimate, x = modeldate)) +
+      geom_point(alpha = 0.25) + geom_smooth(span = 0.25, se = TRUE, color = "orange") +
+      geom_hline(aes(yintercept = mean(approve_estimate, na.rm = TRUE)), color = "darkred", alpha = .33) +
+      scale_x_date(date_breaks = "3 months", date_labels = "%y-%m-%d") +
+      ylim(ylim) +
+      xlim(input$date) +
+      xlab("Date") +
+      ylab("FiveThirtyEight Approval Estimate") +
+      theme(axis.text.x = element_text(angle = 60, hjust = 1),
+            panel.grid = element_blank(),
+            panel.background = element_blank())
     plot1
   })
 
