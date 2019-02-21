d25ce219518453842e3e98fb71ac7d3fc1d9c280
dpmeltz <45836188+dpmeltz@users.noreply.github.com>
2019-02-21 16:59:49 -0500

    add toddlerTrack

---
+++ toddlerTrack/server.R
@@ -0,0 +1,30 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(readxl)
+
+# Define server logic
+shinyServer(function(input, output) {
+
+  wchart <- read_excel("./growthCharts.xlsx", sheet = "weight")
+  hchart <- read_excel("./growthCharts.xlsx", sheet = "height")
+
+    output$distPlot <- renderPlot({
+
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2]
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+
+  })
+
+})
