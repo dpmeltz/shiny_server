66aa79a8cbf6f56f037c77b47d6a08a736e35dcd
dpmeltz <dpmeltz@gmail.com>
2020-01-26 12:09:25 +0000

    Update Hydration

--- DoHyCalc/server.R
+++ DoHyCalc/server.R
@@ -1,3 +1,4 @@
+# Load Libraries
 library(shiny)
 library(shinydashboard)
 library(ggplot2)
@@ -5,44 +6,57 @@ library(ggplot2)
 # Define server logic
 shinyServer(function(input, output) {
 
+  # Calculations
+  # Load reactive values
   flour <- reactive(input$Flour)
   water <- reactive(input$Water)
   starter <- reactive(input$Starter)
   percent <- reactive(input$StarterHydration)
 
-    output$hydrationText <- renderText({
-      percent <- percent() / 100
+  output$hydrationText <- renderText({
+    percent <- percent() / 100
 
 
-        starterFlour <- starter() * (1 / (percent + 1))
-        starterWater <- starter() * (percent / (percent + 1))
+    # Find flour/water values from starter weight/hydration
+    starterFlour <- starter() * (1 / (percent + 1))
+    starterWater <- starter() * (percent / (percent + 1))
 
-        totalWater <- water() + starterWater
-        totalFlour <- flour() + starterFlour
-        waterPercent <- round((totalWater / totalFlour) * 100, digits = 1)
-print(c("Hydration Percent = ", waterPercent, "%"))
-    })
+    # Find total hydration (all water / all flour)
+    totalWater <- water() + starterWater
+    totalFlour <- flour() + starterFlour
+    waterPercent <- round((totalWater / totalFlour) * 100, digits = 1)
 
-    output$hydrationPlot <- renderPlot({
-      percent <- percent() / 100
+    # Print hydration %
+    print(c("Hydration Percent = ", waterPercent, "%"))
+  })
 
-      starterFlour <- starter() * (1 / (percent + 1))
-      starterWater <- starter() * (percent / (percent + 1))
+  # Hydration graph
+  output$hydrationPlot <- renderPlot({
+    percent <- percent() / 100
 
+    # Find flour/water values from starter weight/hydration
+    starterFlour <- starter() * (1 / (percent + 1))
+    starterWater <- starter() * (percent / (percent + 1))
 
-      col1 <- c("Flour", "Starter Flour", "Water", "Starter Water")
-      col2 <- c(flour(), starterFlour, water(), starterWater)
-      cpall <- c("#fee6ce", "#fdae6b", "#deebf7", "#9ecae1")
-      hydration <- data.frame(Source = col1, Weight = col2)
-      hydration$Source <- factor(hydration$Source, levels = col1)
+    # Table source / weight
+    col1 <- c("Flour", "Starter Flour", "Water", "Starter Water")
+    col2 <- c(flour(), starterFlour, water(), starterWater)
+    cpall <- c("#fee6ce", "#fdae6b", "#deebf7", "#9ecae1")
+    hydration <- data.frame(Source = col1, Weight = col2)
+    hydration$Source <- factor(hydration$Source, levels = col1)
 
-
-      ggplot(hydration, aes(x = 1, y = Weight)) +
-          geom_col(position = 'fill', aes(fill = Source)) +
-          scale_fill_manual(values = cpall)
-    })
-
-
+    # Plot weight/total by source
+    ggplot(hydration, aes(x = 1, y = Weight)) +
+      geom_col(position = "fill", aes(fill = Source)) +
+      scale_fill_manual(values = cpall) +
+      # geom_text() add label for water% at top of water bar
+      theme_minimal() +
+      theme(
+        axis.title = element_blank(),
+        axis.line = element_blank(),
+        axis.ticks = element_blank(),
+        axis.text = element_blank(),
+        panel.grid = element_blank()
+      )
+  })
 })
-
-
--- DoHyCalc/ui.R
+++ DoHyCalc/ui.R
@@ -1,36 +1,46 @@
+# Load libraries
 library(shiny)
 library(shinydashboard)
 
 # Define UI for application that plots child growth
 dashboardPage(
-    dashboardHeader(title = "Dough Hydration"),
+  dashboardHeader(title = "Dough Hydration"),
 
-    dashboardSidebar(
-        sidebarMenu(
-            menuItem("Hydration", tabName = "hydration", icon = icon("tint")),
-            menuItem("Other", tabName = "other", icon = icon("info-circle")),
-            hr(),
-            numericInput("Flour", "Flour (in gr):", min = 0, step = 5, value = 600),
-            numericInput("Water", "Water (in gr):", min = 0, step = 5, value = 400),
-            numericInput("Starter", "Starter (in gr):", min = 0, step = 5, value = 250),
-            sliderInput("StarterHydration", "Starter Hydration %", min = 50, max = 200, step = 5, value = 100)
-        )
-    ),
-
-    dashboardBody(
-        tabItems(
-            # First tab content
-            tabItem(tabName = "hydration",
-                    fluidRow(
-                        box(textOutput("hydrationText"))),
-                    fluidRow(
-                        box(plotOutput(("hydrationPlot")))
-                    )
-            ),
-            tabItem(tabName = "other",
-                    fluidRow(
-                        box("This hydration tool is designed to help estimate the hydration percentage of a sourdough loaf. Hydration is calculated as the weight of water over the weight of flour (not water/total). "))
-        )
+  # Sidebar
+  dashboardSidebar(
+    sidebarMenu(
+      # Settings
+      menuItem("Hydration", tabName = "hydration", icon = icon("tint")),
+      menuItem("Other", tabName = "other", icon = icon("info-circle")),
+      hr(),
+      numericInput("Flour", "Flour (in gr):", min = 0, step = 5, value = 600),
+      numericInput("Water", "Water (in gr):", min = 0, step = 5, value = 400),
+      numericInput("Starter", "Starter (in gr):", min = 0, step = 5, value = 250),
+      sliderInput("StarterHydration", "Starter Hydration %", min = 50, max = 200, step = 5, value = 100)
     )
-)
+  ),
+
+  dashboardBody(
+    tabItems(
+      # Outputs
+      tabItem(
+        tabName = "hydration",
+        # Print hydration percentage
+        fluidRow(
+          box(textOutput("hydrationText"))
+        ),
+        # Graph of hydration
+        fluidRow(
+          box(plotOutput(("hydrationPlot")))
+        )
+      ),
+      tabItem(
+        tabName = "other",
+        # Description of bakers math
+        fluidRow(
+          box("This hydration tool is designed to help estimate the hydration percentage of a sourdough loaf. Hydration is calculated as the weight of water over the weight of flour (not water/total). ")
+        )
+      )
+    )
+  )
 )
