b36bb674371bb16feaa933e92456495c2e329136
dpmeltz <45836188+dpmeltz@users.noreply.github.com>
2020-11-09 14:57:29 -0500

    Color/function update

--- Normalizer/server.R
+++ Normalizer/server.R
@@ -44,37 +44,41 @@ shinyServer(function(input, output) {
     # draw the density graph
     curve <- ggplot(aes(x = xval), data = sample) +
       stat_function(fun = dnorm, args = list(sd = sigma, mean = m),
-                    size = 2, color = "#979797") + #Curve
+                    size = 2, color = "#bbbbbb") + #Curve
       annotate('segment', x = m, xend = m + sigma,
                y = height*0.61, yend = height*0.61,
-               size = 2, alpha = 0.33) + #SD Line
       geom_vline(aes(xintercept = scoreMin), size = 1.5, color = "blue") + #Lower/only score
+               size = 2, color = "#bbbbbb") + #SD Line
+      geom_vline(aes(xintercept = scoreMin), size = 1.5, color = "#172f79") + #Lower/only score
       annotate('segment', x = scoreMin,
                 xend = if
                  (input$direction == "Less Than" | input$direction == "Outside") {
                   scoreMin - (.33*sigma)} else {
                   scoreMin + (.33*sigma)},
-                y = height*1.05, yend = height*1.05, size = 1, color = "blue",
+                y = height*1.05, yend = height*1.05, size = 1, color = "#172f79",
                 arrow = arrow(length = unit(0.33,"cm"))) + #Directional arrow for lower/only score
-      geom_vline(aes(xintercept = m), size = 2, alpha = 0.33) + #Mean line
+      geom_vline(aes(xintercept = m), size = 2, color = "#bbbbbb") + #Mean line
       xlim(m - (4*sigma), m + (4*sigma)) + #Limit x to 4SD +/- from mean
       xlab("Scores") + ylab("") + #X Label
       guides(size = FALSE) + #No legend
       scale_y_continuous(limits = c(0, height*1.1)) + #Set y axis from 0 to 10% more than max of curve
       theme(axis.ticks = element_blank(),
             axis.text.y = element_blank(),
-            panel.background = element_rect(fill = FALSE),
+            plot.background = element_rect(fill = "#eeeeee", color = "#eeeeee"),
+            panel.background = element_rect(fill = "#eeeeee"),
             panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            panel.border = element_blank(),
             legend.position = "hidden")
 
 if (input$direction == "Less Than" | input$direction == "Greater Than") {curve} #If multiple add higher score line
     else {curve +
-        geom_vline(aes(xintercept = scoreMax), size = 1.5, color = "red") +
+        geom_vline(aes(xintercept = scoreMax), size = 1.5, color = "#172f79") +
         annotate('segment', x = scoreMax,
           xend = if
             (input$direction == "Outside") {scoreMax + (.33*sigma)} else {
             scoreMax - (.33*sigma)},
-          y = height*1.05, yend = height*1.05, size = 1, color = "red",
+          y = height*1.05, yend = height*1.05, size = 1, color = "#172f79",
           arrow = arrow(length = unit(0.33,"cm")))}
   })
 
